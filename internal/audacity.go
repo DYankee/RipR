1eb4bc95b2ae8949db65b53f35c60364b4273cbd
zachary geary <zpgeary@gmail.com>
2024-03-03 16:51:58 -0500

    started work on album info retrieval

---
+++ internal/audacity.go
@@ -0,0 +1,92 @@
+package Audacity
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+)
+
+var (
+	TONAME   string
+	FROMNAME string
+	EOL      string
+)
+
+func init() {
+	if runtime.GOOS == "windows" {
+		fmt.Println("pipe-test.go, running on windows")
+		TONAME = `\\.\pipe\ToSrvPipe`
+		FROMNAME = `\\.\pipe\FromSrvPipe`
+		EOL = "\r\n"
+	} else {
+		fmt.Println("pipe-test.go, running on linux or mac")
+		TONAME = fmt.Sprintf("/tmp/audacity_script_pipe.to.%d", os.Getuid())
+		FROMNAME = fmt.Sprintf("/tmp/audacity_script_pipe.from.%d", os.Getuid())
+		EOL = "\n"
+	}
+}
+
+type Connection struct {
+	send    *os.File
+	recieve *os.File
+}
+
+type Audacity struct {
+	connection Connection
+}
+
+// Establish a connection to audacity
+func (a *Audacity) Connect() {
+	fmt.Println("Write to  \"" + TONAME + "\"")
+	if _, err := os.Stat(TONAME); err != nil {
+		fmt.Println(" ..does not exist.  Ensure Audacity is running with mod-script-pipe.")
+		os.Exit(1)
+	}
+	fmt.Println("Read from \"" + FROMNAME + "\"")
+	if _, err := os.Stat(FROMNAME); err != nil {
+		fmt.Println(" ..does not exist.  Ensure Audacity is running with mod-script-pipe.")
+		os.Exit(1)
+	}
+	fmt.Println("-- Both pipes exist.  Good.")
+
+	toFile, err := os.OpenFile(TONAME, os.O_RDWR, os.ModeNamedPipe)
+	if err != nil {
+		fmt.Println("-- Failed to open file to write to:", err)
+		os.Exit(1)
+	}
+	fmt.Println("-- File to write to has been opened")
+	a.connection.send = toFile
+
+	fromFile, err := os.OpenFile(FROMNAME, os.O_RDONLY, os.ModeNamedPipe)
+	if err != nil {
+		fmt.Println("-- Failed to open file to read from:", err)
+		os.Exit(1)
+	}
+	fmt.Println("-- File to read from has now been opened too\r")
+	a.connection.recieve = fromFile
+}
+
+func (a Audacity) Close() {
+	a.connection.recieve.Close()
+	a.connection.send.Close()
+}
+
+// send custom command to audacity. reffer to https://manual.audacityteam.org/man/scripting_reference.html for formatting.
+func (a Audacity) Do_command(command string) (res string) {
+
+	//send command
+	fmt.Println("Send: >>> \n" + command)
+	a.connection.send.Write([]byte(command + EOL))
+
+	//get response
+	scanner := bufio.NewScanner(a.connection.recieve)
+	for scanner.Scan() {
+		text := scanner.Text()
+		res += text
+		if text == "" && len(res) != 0 {
+			break
+		}
+	}
+	return res
+}
--- main.go
+++ main.go
@@ -1,99 +1,57 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"runtime"
+
+	"github.com/michiwend/gomusicbrainz"
 )
-
-var (
-	TONAME   string
-	FROMNAME string
-	EOL      string
-)
-
-func init() {
-	if runtime.GOOS == "windows" {
-		fmt.Println("pipe-test.go, running on windows")
-		TONAME = `\\.\pipe\ToSrvPipe`
-		FROMNAME = `\\.\pipe\FromSrvPipe`
-		EOL = "\r\n"
-	} else {
-		fmt.Println("pipe-test.go, running on linux or mac")
-		TONAME = fmt.Sprintf("/tmp/audacity_script_pipe.to.%d", os.Getuid())
-		FROMNAME = fmt.Sprintf("/tmp/audacity_script_pipe.from.%d", os.Getuid())
-		EOL = "\n"
-	}
-}
-
-type Connection struct {
-	send    *os.File
-	recieve *os.File
-}
-
-type Audacity struct {
-	connection Connection
-}
-
-// Establish a connection to audacity
-func (a *Audacity) connect() {
-	fmt.Println("Write to  \"" + TONAME + "\"")
-	if _, err := os.Stat(TONAME); err != nil {
-		fmt.Println(" ..does not exist.  Ensure Audacity is running with mod-script-pipe.")
-		os.Exit(1)
-	}
-	fmt.Println("Read from \"" + FROMNAME + "\"")
-	if _, err := os.Stat(FROMNAME); err != nil {
-		fmt.Println(" ..does not exist.  Ensure Audacity is running with mod-script-pipe.")
-		os.Exit(1)
-	}
-	fmt.Println("-- Both pipes exist.  Good.")
-
-	toFile, err := os.OpenFile(TONAME, os.O_RDWR, os.ModeNamedPipe)
-	if err != nil {
-		fmt.Println("-- Failed to open file to write to:", err)
-		os.Exit(1)
-	}
-	fmt.Println("-- File to write to has been opened")
-	a.connection.send = toFile
-
-	fromFile, err := os.OpenFile(FROMNAME, os.O_RDONLY, os.ModeNamedPipe)
-	if err != nil {
-		fmt.Println("-- Failed to open file to read from:", err)
-		os.Exit(1)
-	}
-	fmt.Println("-- File to read from has now been opened too\r")
-	a.connection.recieve = fromFile
-}
-
-func (a Audacity) close() {
-	a.connection.recieve.Close()
-	a.connection.send.Close()
-}
-
-// send custom command to audacity. reffer to https://manual.audacityteam.org/man/scripting_reference.html for formatting.
-func (a Audacity) do_command(command string) (res string) {
-
-	//send command
-	fmt.Println("Send: >>> \n" + command)
-	a.connection.send.Write([]byte(command + EOL))
-
-	//get response
-	scanner := bufio.NewScanner(a.connection.recieve)
-	for scanner.Scan() {
-		text := scanner.Text()
-		res += text
-		if text == "" && len(res) != 0 {
-			break
-		}
-	}
-	return res
-}
 
 func main() {
-	audacity := Audacity{}
-	audacity.connect()
-	res := audacity.do_command("Help: ")
-	fmt.Println(res)
+
+	client, err := gomusicbrainz.NewWS2Client(
+		"https://musicbrainz.org/ws/2",
+		"A GoMusicBrainz example",
+		"0.0.1-beta",
+		"http://github.com/michiwend/gomusicbrainz")
+	if err != nil {
+		fmt.Println(err)
+	}
+	album := "let it be"
+	artist := "the beatles"
+	search := fmt.Sprintf(`release:%s, artist:%s`, album, artist)
+	resp, err := client.SearchRelease(search, -1, -1)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var id int
+	for _, release := range resp.Releases {
+		if release.Mediums[0].Format == `12" Vinyl` {
+			id += 1
+			fmt.Printf("%s Name: %-20s  Artist: %-20s  Format %-15s  Release Date: %d Country: %s\n",
+				release.ID,
+				release.Title,
+				release.ArtistCredit.NameCredits[0].Artist.Name,
+				release.Mediums[0].Format,
+				release.Date.Year(),
+				release.CountryCode,
+			)
+		}
+	}
+	choice := resp.Releases[0].ID
+
+	resp2, err := client.LookupRelease(choice)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%s %s",
+		resp2.Title,
+		resp2.ArtistCredit.NameCredits[0].Artist.Name,
+	)
+
+	//audacity code
+	//audacity := a.Audacity{}
+	//audacity.Connect()
+	//res := audacity.Do_command("StoreCursorPosition:")
+	//fmt.Println(res)
 }
