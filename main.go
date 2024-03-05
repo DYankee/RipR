4f331144b7f135a3cdb4c7c50751dd630afc1d3b
zachary geary <zpgeary@gmail.com>
2024-03-05 17:00:20 -0500

    moving work

--- main.go
+++ main.go
@@ -1,33 +1,66 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/michiwend/gomusicbrainz"
 )
 
-func main() {
+type AlbumSearchQuerry struct {
+	album  string
+	artist string
+}
 
-	client, err := gomusicbrainz.NewWS2Client(
+func (a *AlbumSearchQuerry) getAlbumName() {
+	fmt.Scanln(&a.album)
+}
+
+func (a *AlbumSearchQuerry) getArtistName() {
+	fmt.Scanln(&a.artist)
+}
+
+func (a AlbumSearchQuerry) newSearch() {
+	fmt.Println("Please enter the album name")
+	a.getAlbumName()
+	fmt.Println("Please enter the artist name")
+	a.getArtistName()
+}
+
+type Musicbrainz struct {
+	client              *gomusicbrainz.WS2Client
+	ReleaseSearchQuerry AlbumSearchQuerry
+	RleaseSearchRes     *gomusicbrainz.ReleaseSearchResponse
+}
+
+func (m *Musicbrainz) newInst() {
+	var err error
+	m.client, err = gomusicbrainz.NewWS2Client(
 		"https://musicbrainz.org/ws/2",
-		"A GoMusicBrainz example",
+		"RRipper",
 		"0.0.1-beta",
-		"http://github.com/michiwend/gomusicbrainz")
+		"http://github.com/Dyankee/RRipper")
 	if err != nil {
 		fmt.Println(err)
 	}
-	album := "let it be"
-	artist := "the beatles"
-	search := fmt.Sprintf(`release:%s, artist:%s`, album, artist)
-	resp, err := client.SearchRelease(search, -1, -1)
+}
+
+func (m *Musicbrainz) ReleaseSearch() {
+	var err error
+	search := fmt.Sprintf(`release:%s, artist:%s`, m.ReleaseSearchQuerry.album, m.ReleaseSearchQuerry.artist)
+	m.RleaseSearchRes, err = m.client.SearchRelease(search, -1, -1)
 	if err != nil {
 		fmt.Println(err)
 	}
-	var id = -1
-	for _, release := range resp.Releases {
+}
+
+func DisplayReleaseRes(res *gomusicbrainz.ReleaseSearchResponse) {
+	var id int
+	for _, release := range res.Releases {
 		id += 1
 		if release.Mediums[0].Format == `12" Vinyl` {
-			fmt.Printf("%d %s Name: %-20s  Artist: %-20s  Format %-15s  Release Date: %d Country: %s\n",
+			fmt.Printf("%-3d %s Name: %-15s  Artist: %-15s  Format %-15s  Release Date: %d Country: %s\n",
 				id,
 				release.ID,
 				release.Title,
@@ -38,17 +71,60 @@ func main() {
 			)
 		}
 	}
-	choice := resp.Releases[8].ID
+}
 
-	resp2, err := client.LookupRelease(choice)
+func getQuerryterms(s *bufio.Reader) (album string, artist string) {
+	fmt.Println("Please enter the album name")
+	album, err := s.ReadString('\n')
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println("Please enter the artist name")
+	artist, err = s.ReadString('\n')
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Printf("Album: %s Artist %s \n", album, artist)
+	return album, artist
+}
+
+func buildReleaseQuerry(album string, artist string) string {
+	querry := fmt.Sprintf(`release:%s, artist:%s`, album, artist)
+	return querry
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	client, err := gomusicbrainz.NewWS2Client(
+		"https://musicbrainz.org/ws/2",
+		"RRipper",
+		"0.0.1-beta",
+		"http://github.com/Dyankee/RRipper")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("%s %s \n",
-		resp2.Title,
-		resp2.Mediums,
-	)
+	res, err := client.SearchRelease(buildReleaseQuerry(getQuerryterms(reader)), -1, -1)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	DisplayReleaseRes(res)
+
+	//choice := resp.Releases[8-1].ID
+	//resp2, err := Musicbrainz.client.LookupRelease(choice, "media+recordings")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//
+	//fmt.Printf("Track %d: %s Length: %d ID: %s\n",
+	//	resp2.Mediums[0].Tracks[0].Position,
+	//	resp2.Mediums[0].Tracks[0].Recording.Title,
+	//	resp2.Mediums[0].Tracks[0].Length,
+	//	resp2.ID,
+	//)
 
 	//audacity code
 	//audacity := a.Audacity{}
